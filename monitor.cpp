10164ce5f94808ec9c070826ea18820bb1873ec3
Gangatharan9843 <M.Kangatharan@in.bosch.com>
2025-08-01 11:32:07 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,34 +1,33 @@
-from time import sleep
-import sys
+#include "./monitor.h"
+#include <assert.h>
+#include <thread>
+#include <chrono>
+#include <iostream>
+using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-def displayVitalAlert(message):
-  print(message)
-  for i in range(6):
-    print('\r* ', end='')
-    sys.stdout.flush()
-    sleep(1)
-    print('\r *', end='')
-    sys.stdout.flush()
-    sleep(1)
+// Helper function to display alert message and blinking effect
+void showAlert(const char* message) {
+  cout << message << '\n';
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
+}
 
-def isTemperatureOk(temperature):
-  if temperature > 102 or temperature < 95:
-    displayVitalAlert("Temperature is critical!")
-    return False
-  return True
-  
-def isPulseRateOk(pulseRate):
-  if pulseRate < 60 or pulseRate > 100:
-    displayVitalAlert("Pulse Rate is out of range!")
-    return False
-  return True
-  
-def isSpo2Ok(spo2):
-  if spo2 < 90:
-    displayVitalAlert("Oxygen Saturation out of range!")
-    return False
-  return True
-
-
-def vitals_ok(temperature, pulseRate, spo2):
-  return isTemperatureOk(temperature) and isPulseRateOk(pulseRate) and isSpo2Ok(spo2)
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+  if (temperature > 102 || temperature < 95) {
+    showAlert("Temperature is critical!");
+    return 0;
+  }
+  if (pulseRate < 60 || pulseRate > 100) {
+    showAlert("Pulse Rate is out of range!");
+    return 0;
+  }
+  if (spo2 < 90) {
+    showAlert("Oxygen Saturation out of range!");
+    return 0;
+  }
+  return 1;
+}
