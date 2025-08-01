63703a330c85cf8071f1bc13221ce748d08fcd0e
Gangatharan9843 <M.Kangatharan@in.bosch.com>
2025-08-01 11:21:44 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,38 +1,34 @@
-#include "./monitor.h"
-#include <assert.h>
-#include <thread>
-#include <chrono>
-#include <iostream>
-using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+from time import sleep
+import sys
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
-}
+def displayVitalAlert(message):
+  print(message)
+  for i in range(6):
+    print('\r* ', end='')
+    sys.stdout.flush()
+    sleep(1)
+    print('\r *', end='')
+    sys.stdout.flush()
+    sleep(1)
+
+def isTemperatureOk(temperature):
+  if temperature > 102 or temperature < 95:
+    displayVitalAlert("Temperature is critical!")
+    return False
+  return True
+  
+def isPulseRateOk(pulseRate):
+  if pulseRate < 60 or pulseRate > 100:
+    displayVitalAlert("Pulse Rate is out of range!")
+    return False
+  return True
+  
+def isSpo2Ok(spo2):
+  if spo2 < 90:
+    displayVitalAlert("Oxygen Saturation out of range!")
+    return False
+  return True
+
+
+def vitals_ok(temperature, pulseRate, spo2):
+  return isTemperatureOk(temperature) and isPulseRateOk(pulseRate) and isSpo2Ok(spo2)
